8fa2ff86f1a36d85d8601423d7ddcf81c660655e
iagoqueirozx <146447548+iagoqueirozx@users.noreply.github.com>
2024-05-07 02:03:51 -0300

    Update arcazogtm.js

--- arcazogtm.js
+++ arcazogtm.js
@@ -1,8 +1,4 @@
-
- async src="https://www.googletagmanager.com/gtag/js?id=AW-11005044618"
-
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'AW-11005044618');
